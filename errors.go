5329dfd70faff14ab56069188e68541961e0dd7f
Johan Fogelström <zapmew@gmail.com>
2015-03-09 17:03:56 +0100

    Added a error to symbolize a faulty directory path for plugin loading.

--- errors.go
+++ errors.go
@@ -8,4 +8,5 @@ var (
 	NoSupportedFormat = errors.New("No supported formats in common")
 	Unblocking        = errors.New("This is a unblocking recieve")
 	NotImplemented    = errors.New("Operation is not implemented")
+	NotDirectory      = errors.New("Path supplied is not a directory")
 )
