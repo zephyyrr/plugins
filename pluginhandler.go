c2d9ec36e4678d2ed7df227ebc58f4987afa1efc
Johan Fogelström <zapmew@gmail.com>
2015-03-09 15:57:10 +0100

    Wrote a plugin handler. It is more of a event emitter thingy, but that is not important

---
+++ pluginhandler.go
@@ -0,0 +1,133 @@
+package plugins
+
+import (
+	"reflect"
+	"runtime"
+	"time"
+)
+
+type PluginHandler struct {
+	plugins       plugins
+	closers       map[string]chan<- struct{}
+	subscriptions map[Event]plugins
+	selects       []reflect.SelectCase
+}
+
+type plugins map[string]Plugin
+
+func NewHandler() *PluginHandler {
+	return &PluginHandler{
+		plugins:       make(plugins),
+		closers:       make(map[string]chan<- struct{}),
+		subscriptions: make(map[Event]plugins),
+		selects:       make([]reflect.SelectCase, 0, 16),
+	}
+}
+
+type packet struct {
+	Event Event
+	Args  Args
+}
+
+func (ph *PluginHandler) Handle(pl Plugin) {
+	ph.plugins[pl.Name()] = pl
+
+	for _, sub := range pl.Subscribes() {
+		if ph.subscriptions[sub] == nil {
+			ph.subscriptions[sub] = make(plugins)
+		}
+		ph.subscriptions[sub][pl.Name()] = pl
+	}
+
+	f, ch, closer := generatefunc(pl)
+	ph.closers[pl.Name()] = closer
+
+	go f()
+
+	rch := reflect.ValueOf(ch)
+	ph.selects = append(ph.selects,
+		reflect.SelectCase{
+			Dir:  reflect.SelectRecv,
+			Chan: rch,
+		})
+
+}
+
+func generatefunc(pl Plugin) (func(), <-chan packet, chan<- struct{}) {
+	ch, closer := make(chan packet), make(chan struct{})
+	return func() {
+		defer close(ch)
+
+		if len(pl.Provides()) == 0 {
+			return //Nothing is provided, so nothing to listen for.
+		}
+
+		for {
+			select {
+			case <-closer:
+				return
+			default:
+				event, args, err := pl.Recieve() //Probably blocking. Hopefully not for to long.
+				if err != nil {
+					switch err {
+					case Unblocking:
+						time.Sleep(500 * time.Millisecond)
+					case NotImplemented:
+					default:
+						return
+					}
+					continue
+				}
+				ch <- packet{event, args}
+			}
+			runtime.Gosched() // Allow something else to run if tight loop.
+		}
+	}, ch, closer
+}
+
+func (ph *PluginHandler) Unload(pl Plugin) {
+	close(ph.closers[pl.Name()])
+	delete(ph.closers, pl.Name())
+
+	for _, sub := range pl.Subscribes() {
+		delete(ph.subscriptions[sub], pl.Name())
+	}
+
+	delete(ph.plugins, pl.Name())
+}
+
+func (ph *PluginHandler) ListenAndServe() error {
+
+	for len(ph.plugins) > 0 {
+		if chosen, recv, ok := reflect.Select(ph.selects); ok {
+			pck := recv.Interface().(packet)
+			ph.Dispatch(pck.Event, pck.Args)
+		} else {
+			//Not ok
+			//Removing select case from list
+			if chosen == len(ph.selects)-1 {
+				ph.selects = ph.selects[:len(ph.selects)-1]
+			} else {
+				ph.selects = append(ph.selects[:chosen], ph.selects[chosen+1:]...)
+			}
+		}
+	}
+
+	return nil
+}
+
+func (ph PluginHandler) Dispatch(e Event, args Args) error {
+	return ph.dispatch("local", e, args)
+}
+
+func (ph PluginHandler) dispatch(identifier string, e Event, args Args) (err error) {
+	for _, plugin := range ph.subscriptions[e] {
+		go func(plugin Plugin) {
+			tmp := plugin.Send(e, args)
+			if err == nil && tmp != NotImplemented {
+				err = tmp
+			}
+		}(plugin)
+	}
+	return
+}
