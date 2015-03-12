60870998f3a9d67663739f211faeafc139b012dc
Johan Fogelström <zapmew@gmail.com>
2015-03-12 12:34:05 +0100

    Updated the example to match the new names

--- example/main.go
+++ example/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"github.com/zephyyrr/plugins"
 )
 
 func main() {
-	ph := plugins.NewHandler()
-	ph.Handle(PrintPlugin{})
+	ph := plugins.NewManager()
 	ph.Handle(MessangerPlugin{})
+
+	if plugins, err := plugins.LoadAll("./plugins"); err != nil {
+		ph.HandleAll(plugins...)
+	}
+
+	ph.Muxer().AddHandler("message", plugins.HandlerFunc(func(event plugins.Event, args plugins.Args) {
+		fmt.Println(args)
+	}))
 
 	ph.ListenAndServe()
 }
